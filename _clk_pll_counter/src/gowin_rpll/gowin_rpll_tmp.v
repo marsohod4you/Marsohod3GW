99111f1c715851e7bb2d9e3f3ef6fec9467db8f3
marsohod4you <nck.kovach@gmail.com>
2023-05-10 17:50:13 +0300

    add binary counter with moving bit project, also project uses PLL

---
+++ _clk_pll_counter/src/gowin_rpll/gowin_rpll_tmp.v
@@ -0,0 +1,18 @@
+//Copyright (C)2014-2022 Gowin Semiconductor Corporation.
+//All rights reserved.
+//File Title: Template file for instantiation
+//GOWIN Version: V1.9.8.09
+//Part Number: GW1NR-UV9QN88PC6/I5
+//Device: GW1NR-9
+//Created Time: Sun May 07 16:12:01 2023
+
+//Change the instance name and port connections to the signal names
+//--------Copy here to design--------
+
+    Gowin_rPLL your_instance_name(
+        .clkout(clkout_o), //output clkout
+        .lock(lock_o), //output lock
+        .clkin(clkin_i) //input clkin
+    );
+
+//--------Copy end-------------------
---
+++ _clk_pll_counter/src/top.v
@@ -0,0 +1,53 @@
+
+module top(
+	input  CLK, KEY0, KEY1,
+	input  [7:0] ADC_D,
+	input  [7:0] FTD,
+	input  [7:0] FTC,
+	input  FTB0,
+	input  FTB2,
+	output FTB1,
+	output FTB3,
+	output ADC_CLK,
+	output [7:0] LED,
+	output [18:0] IO,
+	output       TMDS_CLK_N,
+	output       TMDS_CLK_P,
+	output [2:0] TMDS_D_N,
+	output [2:0] TMDS_D_P
+);
+
+wire pll_out_clk;
+wire pll_locked;
+Gowin_rPLL rpll(
+    .clkin( CLK ),
+    .clkout( pll_out_clk ),
+    .lock( pll_locked )
+    );
+
+reg [28:0]cnt = 0;
+reg [7:0]moving_bit;
+always @( posedge pll_out_clk )
+begin
+		cnt <= cnt + 1;
+        moving_bit <= cnt[26] ? 
+                            8'h01 << cnt[25:23] :
+                            8'h80 >> cnt[25:23] ;
+end
+
+assign LED = KEY0 ? cnt[28:21] : moving_bit;
+
+//Serial_RX -> Serial_TX
+assign FTB1 = FTB0;
+assign FTB3 = FTB2;
+
+assign IO = 0;
+
+assign ADC_CLK = 1'b0;
+
+assign TMDS_CLK_N = 1'b0;
+assign TMDS_CLK_P = 1'b0;
+assign TMDS_D_N   = 4'd0;
+assign TMDS_D_P   = 4'd0;
+
+endmodule
