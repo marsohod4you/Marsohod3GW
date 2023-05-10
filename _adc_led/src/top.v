943a9920f371e8981764b8c458db9beb6ba4e2c4
marsohod4you <nck.kovach@gmail.com>
2023-05-10 20:01:00 +0300

    add FPGA project capturing ADC1175 data and show on 8 bit LEDs. Connect resistive divider to ADC input to vary LEDs values

---
+++ _adc_led/src/top.v
@@ -0,0 +1,50 @@
+
+module top(
+	input  CLK, KEY0, KEY1,
+	input  [7:0] ADC_D,
+	input  [7:0] FTD,
+	input  [7:0] FTC,
+	input  FTB0,
+	input  FTB2,
+	output FTB1,
+	output FTB3,
+	output ADC_CLK,
+	output [7:0] LED,
+	output [18:0] IO,
+	output       TMDS_CLK_N,
+	output       TMDS_CLK_P,
+	output [2:0] TMDS_D_N,
+	output [2:0] TMDS_D_P
+);
+
+wire pll_out_clk;
+wire pll_locked;
+Gowin_rPLL rpll(
+    .clkin( CLK ),
+    .clkout( pll_out_clk ),
+    .lock( pll_locked )
+    );
+
+//pass clk to external ADC chip
+assign ADC_CLK = pll_out_clk;
+
+//capture ADC data
+reg [7:0]adc_data;
+always @(posedge pll_out_clk)
+    adc_data <= ADC_D;
+
+//show ADC data on LEDs
+assign LED = adc_data;
+
+//Serial_RX -> Serial_TX
+assign FTB1 = FTB0;
+assign FTB3 = FTB2;
+
+assign IO = 0;
+
+assign TMDS_CLK_N = 1'b0;
+assign TMDS_CLK_P = 1'b0;
+assign TMDS_D_N   = 4'd0;
+assign TMDS_D_P   = 4'd0;
+
+endmodule
